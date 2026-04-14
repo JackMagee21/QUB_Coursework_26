a08fa949154ac77536cecc83f766e838a4a0389a
JackMagee21 <mt.j07@yahoo.com>
2026-04-14 15:12:17 +0100

    got report 5 done + finised the test data for the db

--- main/js/SQL_possible_methods/test.js
+++ main/js/SQL_possible_methods/test.js
@@ -11,13 +11,19 @@ const AddTestResult = async (Student_ID, Test_ID, Grade, Pass_Fail) => {
 
 
 const getAllTestIdsBasedOnStudent = (Student_ID) => {
-    const sql = `SELECT Test_ID FROM Test_Student WHERE Student_ID = ${escapeSql(Student_ID)}`;
+    const sql = `SELECT Test_ID, Score FROM Test_Student WHERE Student_ID = ${escapeSql(Student_ID)}`;
     return runQuery(sql);
 }
 
 
+
 const getStudentMarksForTest = async (Student_ID) => {
-    const sql = `SELECT Student_ID, Grade FROM Test_Student WHERE Student_ID = ${escapeSql(Student_ID)}`;
+    const sql = `SELECT Student_ID, Score FROM Test_Student WHERE Student_ID = ${escapeSql(Student_ID)}`;
+    return await runQuery(sql);
+}
+
+getTestFromId = async (Test_ID) => {
+    const sql = `SELECT Name FROM Test WHERE Test_ID = ${Test_ID}`;
     return await runQuery(sql);
 }
 
--- main/js/reports/Jack/report.js
+++ main/js/reports/Jack/report.js
@@ -17,7 +17,7 @@ const getAllStudentsWithAssistanceNoDups = async () => {
 }
 
 const getAllStudentsWithoutAssistanceNoDups = async () => {
-    const sql = `SELECT s.Student_ID, s.Forename, s.Surname, GROUP_CONCAT(DISTINCT a.Assistance_Name SEPARATOR ', ') AS Assistance_Name FROM Student s JOIN Student_Disability_Support sds ON s.Student_ID = sds.Student_ID JOIN Assistance a ON sds.Assistance_ID = a.Assistance_ID WHERE sds.Assistance_ID IS NULL GROUP BY s.Student_ID, s.Forename, s.Surname`;
+    const sql = `SELECT s.Student_ID, s.Forename, s.Surname FROM Student s LEFT JOIN Student_Disability_Support sds ON s.Student_ID = sds.Student_ID WHERE sds.Student_ID IS NULL`;
     const result = await runQuery(sql);
     return result;
 }
